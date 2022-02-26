55a323e49f203d67490cacdc9a6af003e9194319
Reynadi Andreas Priesley <rey@reynadi.com>
2022-02-26 10:40:31 +0700

    feat: config setup

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
   pwa: {
-    disable:
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "preview" ||
-      process.env.NODE_ENV === "production",
+    disable: process.env.NODE_ENV === "development",
     // delete two lines above to enable PWA in production deployment
     // add your own icons to public/manifest.json
     // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
@@ -17,5 +14,32 @@ module.exports = withPWA({
   reactStrictMode: true,
   eslint: {
     dirs: ["src"],
+  },
+  swcMinify: true,
+
+  webpack(config, { dev, isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            typescript: true,
+            icon: true,
+          },
+        },
+      ],
+    });
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
   },
 });
