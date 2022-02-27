50ff5f729c0cb5ebd3265de0abe679216a133608
Reynadi Andreas Priesley <rey@reynadi.com>
2022-02-27 20:03:34 +0700

    chore: current work

--- src/lib/components/bookmarks/Main.tsx
+++ src/lib/components/bookmarks/Main.tsx
@@ -1,9 +1,46 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+import CatalogBox from "../search/CatalogBox";
+import type { IBookmark } from "lib/utils/bookmarks";
+import { getBookmarks } from "lib/utils/bookmarks";
 
 function Main() {
+  const [bookmarkshare, setBookmarkshere] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const { status, bookmarks } = await getBookmarks(
+        String(window.localStorage.getItem("access_token"))
+      );
+      if (status === 200) {
+        // console.log(bookmarks);
+        setBookmarkshere(bookmarks);
+        setLoading(false);
+      }
+    })();
+  }, [setLoading, setBookmarkshere]);
   return (
     <Box>
       <Heading>Your bookmarks</Heading>
+      <Box>
+        {!isLoading ? (
+          <SimpleGrid columns={[2, null, 3]} spacing="12">
+            {bookmarkshare.map((x: IBookmark) => (
+              <CatalogBox
+                item={{
+                  title: x.bookmarkname,
+                  link: x.bookmarkurl,
+                  displayLink: x.bookmarkurl,
+                  snippet: x.bookmarkdescription,
+                }}
+              />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <Text>Loading</Text>
+        )}
+      </Box>
     </Box>
   );
 }
