d73b6f2c7f3e6e48d5c4f6966926a9ea46a9b59a
Reynadi Andreas Priesley <rey@reynadi.com>
2022-02-27 17:17:49 +0700

    chore: added url decode to query search

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,6 +1,6 @@
 export const API_URI = String(process.env.API_URI);
 
 export const SearchQueryAPI = (q: string) =>
-  `${API_URI}/v1/query/search?q=${q}`;
+  `${API_URI}/v1/query/search?q=${encodeURIComponent(q)}`;
 
 export const RetriveGoogleOauth = String(process.env.OAUTH_URL);
